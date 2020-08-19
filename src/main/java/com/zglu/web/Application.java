444cd59ec985b0c9052cd921ec8eec8486d7a895
zhigen <385861131@qq.com>
2020-08-19 17:44:01 +0800

    Initial commit

---
+++ src/main/java/com/zglu/web/Application.java
@@ -0,0 +1,16 @@
+package com.zglu.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zglu
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
